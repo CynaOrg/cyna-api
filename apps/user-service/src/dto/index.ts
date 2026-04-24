29d87893b39eddb7095a3ad9b229bf15c6ec4173
iliesmahoudeau <ilies.mahoudeau@icloud.col>
2026-04-24 12:22:52 +0200

    feat(user-service): add create/update UserAddress DTOs

--- apps/user-service/src/dto/index.ts
+++ apps/user-service/src/dto/index.ts
@@ -1,2 +1,4 @@
 export * from './create-user.dto';
 export * from './admin-update-status.dto';
+export { CreateUserAddressDto } from './create-user-address.dto';
+export { UpdateUserAddressDto } from './update-user-address.dto';
---
+++ apps/user-service/src/dto/update-user-address.dto.ts
@@ -0,0 +1,5 @@
+// cyna-api/apps/user-service/src/dto/update-user-address.dto.ts
+import { PartialType } from '@nestjs/swagger';
+import { CreateUserAddressDto } from './create-user-address.dto';
+
+export class UpdateUserAddressDto extends PartialType(CreateUserAddressDto) {}
